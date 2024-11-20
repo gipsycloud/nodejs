b6f166c5116a7d89e483dac9abbe9c38e323e8a6
webgipsy <babybird1801@gmail.com>
2024-11-20 10:53:44 +0630

    adding calculator function

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 npm install
 npm start
+
+## Calculator functions
+npm i body-parser --
--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-let http  = require('http');
-let start = (req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  if (req.method == 'GET') {
-    res.end('Hello, World!, Get request');
-  } else {
-    res.end('Hello, World!, Post request');
-  }
-}
-let server  = http.createServer(start);
+const express = require('express');
+
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/', function(req, res) {
+  // console.log(req.body.num1, req.body.num2);
+  const num1 = req.body.num1;
+  const num2 = req.body.num2;
+  const result = Number(num1) + Number(num2);
+  res.send("" + result);
+});
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});

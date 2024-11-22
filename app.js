568d5c8219df26d2089f911cd31e8625bdc3e766
webgipsy <babybird1801@gmail.com>
2024-11-22 12:43:11 +0630

    adding mogodb

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3000;
 const coronaRoute = require('./routes/virus');
 const app = express();
+
+mongoose.connect('mongodb+srv://lesson:lesson123@lesson.mudrs.mongodb.net/?retryWrites=true&w=majority&appName=lesson')
+  .then(() => console.log('Connected!'))
+  .catch((err) => {
+    console.log(err);
+  });
 
 // app.get('/', (req, res) => {
 //   // res.send('Hello, World!');
@@ -9,12 +16,16 @@ const app = express();
 // });
 
 // middleware
-app.use('/', (req, res, next) => {
-  console.log('/ middleware');
-  next();  // async function
+// app.use('/', (req, res, next) => {
+//   console.log('/ middleware');
+//   next();  // async function
   
-});
+// });
+
+// mongoose.connect("mongodb+srv://lesson:lesson123@lesson.mudrs.mongodb.net/?retryWrites=true&w=majority&appName=lesson", 
+//   () => console.log("DB connected.")
+  
+// );
 
 app.use('/api/coronas', coronaRoute);
-
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`));

f5ada92e399caf60fccca681bf298710282592a8
webgipsy <babybird1801@gmail.com>
2024-12-13 16:10:33 +0630

    CRUD function with mysql database in node js

--- mysql.js
+++ mysql.js
@@ -6,7 +6,8 @@ const mysql = require('mysql');
 var con = mysql.createConnection({
   host: "localhost",
   user: "root", // WRONG USER
-  password: "password"
+  password: "password",
+  database: "simplilearn"
 });
 
 con.connect(function (err) {
@@ -19,12 +20,64 @@ con.on('error', function (err) {
 });
 
 const app = express();
+
+app.get('/', function (req, res) {
+  res.send('Welcome');
+});
+
 app.get('/createdb', (req, res) => {
   /*Create a database named "mydb":*/
   con.query("CREATE DATABASE simplilearn", function (err, result) {
     if (err) throw err;
     console.log("Database created");
     res.send('Database created');
+  });
+});
+
+//create table
+app.get('/table', (req, res) => {
+  con.query("CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), designation VARCHAR(255))", function (err, result) {
+    if (err) throw err;
+    console.log("Table created");
+    res.send('Table created');
+  });
+});
+
+// Insert a new user
+app.get('/userone', (req, res) => {
+  con.query("INSERT INTO users (name, designation) VALUES ('John Doe', 'Software Engineer')", function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+    res.send('1 record inserted');
+  });
+});
+// select a user
+app.get('/getuser', (req, res) => {
+  con.query("SELECT * FROM users", function (err, result) {
+    if (err) throw err;
+    console.log(result);
+    res.json(result);
+  });
+});
+
+// update a userupdateuser
+app.get('/updateuser/:id', (req, res) => {
+  let newName = 'Updated name';
+  let sql = `UPDATE users SET name = '${newName}' WHERE id = ${req.params.id}`;
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log(`1 record updated`);
+    res.send('1 record updated');
+  });
+});
+
+// delete a user
+app.get('/deleteuser/:id', (req, res) => {
+  let sql = `DELETE FROM users WHERE id = ${req.params.id}`;
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log(`1 record deleted`);
+    res.send('1 record deleted');
   });
 });
 

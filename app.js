da06a0c35dafcce6611fb83cee854887fa31762f
webgipsy <babybird1801@gmail.com>
2024-12-25 18:50:42 +0630

    create for tutorial

--- app.js
+++ app.js
@@ -1,39 +1,32 @@
+require('dotenv').config();
 const express = require('express');
+const expressLayout = require('express-ejs-layouts');
 const mongoose = require('mongoose');
-const coronaRoute = require('./routes/virus');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
-// const ProgressBar = require('progress');
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static('public'));
+app.use(expressLayout);
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
+
+app.use('/', require('./routes/main'));
+
 // Middleware
-app.use(cors());
-app.use(bodyParser.json());
+// app.use(cors());
+// app.use(bodyParser.json());
 
 // Mogodb connection
-mongoose.connect('mongodb://localhost:27017')
-  .then(() => console.log('Connected! app'))
-  .catch((err) => {
-    console.log(err);
-  });
+// mongoose.connect('mongodb://localhost:27017')
+//   .then(() => console.log('Connected! app'))
+//   .catch((err) => {
+//     console.log(err);
+//   });
 
 var ProgressBar = require('./progressbar.js');
 
 // mongodb+srv://lesson:lesson123@lesson.mudrs.mongodb.net/?retryWrites=true&w=majority&appName=lesson
-// app.get('/', (req, res) => {
-//   // res.send('Hello, World!');
-//   res.json({country: 'USA'});
-// });
-
-// middleware
-// app.use('/', (req, res, next) => {
-//   console.log('/ middleware');
-//   next();  // async function
-
-// });
-
-// );
 // app.use(express.json());
-app.use('/api/coronas', coronaRoute);
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`));

a7ade96231e2b69dcf84f34bc044d7770735b275
webgipsy <babybird1801@gmail.com>
2025-01-03 10:40:58 +0630

    user registration

--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const adminLayout = './layouts/blog/admin';
 
@@ -13,6 +15,45 @@ router.get('/admin', async (req, res) => {
     }
 
     res.render('admin/index', { locals, layout: adminLayout });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.post('/admin', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    if (req.body.username === 'admin' && req.body.password == 'password') {
+      res.send('You are logged in');
+    } else {
+      res.send('Invalid credentials');
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// register a new user
+router.post('/register', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    // console.log(hashedPassword.toString(), req.body);
+    try {
+      const user = new User({
+        email: req.body.email,
+        username: req.body.username,
+        password: hashedPassword
+      });
+      console.log(user);
+      await user.save();
+      res.status(201).json({ message: 'User created', user });
+    } catch (err) {
+      if (err.code === 11000) {
+        res.send('User already exists');
+      }
+      res.status(500).send('Server error');
+    }
   } catch (err) {
     console.log(err);
   }

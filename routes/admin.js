64b3fd408c3be4f1cc40a8ad58deaddf9fce5840
webgipsy <babybird1801@gmail.com>
2025-01-03 17:12:19 +0630

    user login with jwt token

--- routes/admin.js
+++ routes/admin.js
@@ -6,6 +6,23 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 const adminLayout = './layouts/blog/admin';
+const jwtSecret = process.env.JWT_SECRET;
+
+// middleware to check if user is logged in
+const authMiddleware = (req, res, next) => {
+  const token = req.cookies.token; // get the token from the brower cookies
+  // console.log(req.cookies.token);
+  if (!token) {
+    return res.redirect('/admin', { message: 'You must be logged in' });
+  }
+  const decodedToken = jwt.verify(token, jwtSecret); // decode the token to get the user id and secret from the token object returned by jwt.verify method of the jsonwebtoken package
+  const user = User.findById(decodedToken.userId);
+  if (!user) {
+    return res.redirect('/admin');
+  }
+  req.user = user;
+  next();
+};
 
 router.get('/admin', async (req, res) => {
   try {
@@ -23,14 +40,25 @@ router.get('/admin', async (req, res) => {
 router.post('/admin', async (req, res) => {
   try {
     const { username, password } = req.body;
-    if (req.body.username === 'admin' && req.body.password == 'password') {
-      res.send('You are logged in');
-    } else {
-      res.send('Invalid credentials');
+    const user = await User.findOne({ username: username });
+    if (!user) {
+      return res.status(404).send({ message: 'Invalid credntials' });
     }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(404).send({ message: 'Invalid credntials' });
+    }
+    const token = jwt.sign({ userId: user._id }, jwtSecret);
+    res.cookie('token', token, { httpOnly: true });
+
+    res.redirect('/dashboard');
   } catch (err) {
     console.log(err);
   }
+});
+
+router.get('/dashboard', authMiddleware, async (req, res) => {
+  res.render('admin/dashboard', { title: 'Dashboard', description: 'Welcome to the admin panel' });
 });
 
 // register a new user
@@ -58,5 +86,16 @@ router.post('/register', async (req, res) => {
     console.log(err);
   }
 });
+
+// login a user
+router.get('/login', (req, res) => {
+  res.render('admin/index', { title: 'Admin', description: 'Welcome to my blog' });
+});
+
+
+router.get('/register', (req, res) => {
+  res.render('admin/register', { title: 'Admin Register', description: 'Welcome to my blog' });
+});
+
 
 module.exports = router;
